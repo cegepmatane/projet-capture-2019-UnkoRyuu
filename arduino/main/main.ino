ea4633a6503db8f1125ab6e0842dc1c77965c4f8
Q.B UnkoRyuu <54322013+UnkoRyuu@users.noreply.github.com>
2019-11-11 17:12:31 -0500

    code de l'arduino

---
+++ arduino/main/main.ino
@@ -0,0 +1,60 @@
+int DHpin = 8; 
+byte dat[5];   
+
+byte read_data()
+{
+  byte i = 0;
+  byte result = 0;
+  for (i = 0; i < 8; i++) {
+      while (digitalRead(DHpin) == LOW); 
+      delayMicroseconds(30); 
+      if (digitalRead(DHpin) == HIGH)
+        result |= (1 << (8 - i)); 
+    while (digitalRead(DHpin) == HIGH); 
+    }
+  return result;
+}
+
+void start_test()
+{
+  digitalWrite(DHpin, LOW);
+  delay(30); 
+  digitalWrite(DHpin, HIGH);
+  delayMicroseconds(40);
+  pinMode(DHpin, INPUT);
+  while(digitalRead(DHpin) == HIGH);
+  delayMicroseconds(80); 
+  
+  if(digitalRead(DHpin) == LOW)
+    delayMicroseconds(80); 
+  for(int i = 0; i < 5; i++) 
+    dat[i] = read_data();
+  pinMode(DHpin, OUTPUT);
+  digitalWrite(DHpin, HIGH); 
+}
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(DHpin, OUTPUT);
+}
+
+void loop()
+{
+  start_test();
+  Serial.print("Humdity = ");
+  Serial.print(dat[0], DEC); 
+  Serial.print('.');
+  Serial.print(dat[1], DEC); 
+  Serial.println('%');
+  Serial.print("Temperature = ");
+  Serial.print(dat[2], DEC);
+  Serial.print('.');
+  Serial.print(dat[3], DEC); 
+  Serial.println('C');
+  byte checksum = dat[0] + dat[1] + dat[2] + dat[3];
+  if (dat[4] != checksum) 
+    Serial.println("-- Checksum Error!");
+  else
+    Serial.println("-- OK"); 
+  delay(60000);
+}
