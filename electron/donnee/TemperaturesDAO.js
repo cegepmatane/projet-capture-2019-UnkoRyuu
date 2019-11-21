5ea7fb9746226d95e1fe8cf9437134cb21a0c0eb
Lucas <lucas.portal@orange.fr>
2019-11-21 16:23:56 -0500

    Update TemperaturesDAO.js

--- electron/donnee/TemperaturesDAO.js
+++ electron/donnee/TemperaturesDAO.js
@@ -1,3 +1,8 @@
 var TemperartureDAO = function(){
-  
+  connection.query('SELECT * FROM `temperature` WHERE 1', function(err, rows, fields) {
+  if (err) throw err;
+    for (var i = 0; i < rows.length; i++) {
+      result = rows; //je stock le résultat dans une variable pour l'envoyer à la vue
+    };
+});
 }
