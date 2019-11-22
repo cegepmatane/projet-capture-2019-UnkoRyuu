26bb2496600e4cf0b4890d5958e7dd2dc37a8f7b
Thorekt <35566455+Thorekt@users.noreply.github.com>
2019-11-22 00:14:54 -0500

    liste jour full fix php

--- php-serveur/listerTemperatureJour.php
+++ php-serveur/listerTemperatureJour.php
@@ -6,16 +6,16 @@ function ListeTemperatureParAnneeMoisJour($annee, $mois, $jour){
   $req = $bdd->prepare( 'SELECT AVG(temperature) AS "temperatureMoy",
                         MIN(temperature) AS "temperatureMin",
                         MAX(temperature) AS "temperatureMax",
-                        extract(hour from "dateReleve") AS heure,
+                        extract(hour from "dateReleve") AS heure
                         FROM "ReleveEnvironnement"
-                        WHERE extract(year from "dateReleve") = ?,
-                        extract(month from "dateReleve") = ?,
-                        extract(day from "dateReleve") = ?
-                        GROUP BY (hour from "dateReleve")
-                        ORDER BY (hour from "dateReleve"); ');
+                        WHERE extract(year from "dateReleve") = ?
+                        AND extract(month from "dateReleve") = ?
+                        AND extract(day from "dateReleve") = ?
+                        GROUP BY extract(hour from "dateReleve")
+                        ORDER BY extract(hour from "dateReleve"); ');
 
   $req->execute(array($annee, $mois, $jour));
-  return $req->fetch(PDO::FETCH_BOTH);
+  return $req->fetchAll();
 }
 
 function MoyMinMaxParAnneeMoisJour($annee, $mois, $jour){
@@ -24,46 +24,44 @@ function MoyMinMaxParAnneeMoisJour($annee, $mois, $jour){
                         MIN(temperature) AS "temperatureMin",
                         MAX(temperature) AS "temperatureMax"
                         FROM "ReleveEnvironnement"
-                        WHERE extract(year from "dateReleve") = ?,
-                        extract(month from "dateReleve") = ?,
-                        extract(day from "dateReleve") = ?;');
+                        WHERE extract(year from "dateReleve") = ?
+                        AND extract(month from "dateReleve") = ?
+                        AND extract(day from "dateReleve") = ?;');
 
   $req->execute(array($annee, $mois, $jour));
   return $req->fetch(PDO::FETCH_BOTH);
 }
 
-$reponse = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
-
-echo   $_GET['jour']."/". $_GET['mois'] ."/". $_GET['annee'];
+$reponse = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n\n";
 
 
 if (isset($_GET['annee'],$_GET['mois'],$_GET['jour']))
 {
 
 
-  $reponse .= "<ListeTemperature date= '" . $_GET['jour']."/". $_GET['mois'] ."/". $_GET['annee']  ."'>";
+  $reponse .= "<ListeTemperature date= '" . $_GET['jour']."/". $_GET['mois'] ."/". $_GET['annee']  ."'>\n";
 
   $listeTemperature = ListeTemperatureParAnneeMoisJour($_GET['annee'],$_GET['mois'],$_GET['jour']);
 
-  foreach ($listeTemperature as $key => $value) {
-    $reponse .= "<Temperature heure='".$heure."'>";
-      $reponse .= "<Min>".$temperatureMin."</Min>";
-      $reponse .= "<Max>".$temperatureMax."</Max>";
-      $reponse .= "<Moyenne>".$temperatureMoy."</Moyenne>";
-    $reponse .= "</Temperature>";
-  }
+  if (sizeof($listeTemperature) !=  0  ) {
+    foreach ($listeTemperature as $key ) {
+      $reponse .= "<Temperature heure='" . $key['heure'] . "'>\n";
+        $reponse .= "<Min>".$key['temperatureMin']."</Min>\n";
+        $reponse .= "<Max>".$key['temperatureMax']."</Max>\n";
+        $reponse .= "<Moyenne>".$key['temperatureMoy']."</Moyenne>\n";
+      $reponse .= "</Temperature>\n";
+    }
 
-  if ($listeTemperature != null) {
     $resultat = MoyMinMaxParAnneeMoisJour($_GET['annee'],$_GET['mois'],$_GET['jour']);
     if ($resultat != null) {
-      $reponse .= "<MinTotal>".$resultat->temperatureMin."</MinTotal>";
-      $reponse .= "<MaxTotal>".$resultat->temperatureMax."</MaxTotal>";
-      $reponse .= "<MoyenneTotal>".$resultat->temperatureMoy."</MoyenneTotal>";
+      $reponse .= "<MinTotal>".$resultat['temperatureMin']."</MinTotal>\n";
+      $reponse .= "<MaxTotal>".$resultat['temperatureMax']."</MaxTotal>\n";
+      $reponse .= "<MoyenneTotal>".$resultat['temperatureMoy']."</MoyenneTotal>\n";
     }
   }
 
 
-  $reponse .="</ListeTemperature>";
+  $reponse .="</ListeTemperature>\n";
 
 }
 echo $reponse;
